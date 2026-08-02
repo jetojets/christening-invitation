e29bcf645adb9db2c571c5ec29ec0c2c72ba1d35
jetojets <jetomarababol@gmail.com>
2026-08-02 21:54:30 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,327 @@
+console.log(document.getElementById("openInvitation"));
+
+document.addEventListener("DOMContentLoaded",()=>{
+
+
+// ELEMENTS
+
+const openingScreen = document.getElementById("opening-screen");
+const openInvitation = document.getElementById("openInvitation");
+
+const music = document.getElementById("bgMusic");
+const musicBtn = document.getElementById("musicBtn");
+
+const navbar = document.getElementById("navbar");
+
+
+// OPEN INVITATION
+
+if(openInvitation){
+
+    openInvitation.addEventListener("click",()=>{
+
+
+        if(openingScreen){
+
+            openingScreen.classList.add("hide");
+
+        }
+
+
+        if(navbar){
+
+            navbar.classList.add("show");
+
+            const hero = document.querySelector(".hero");
+
+      if(hero){
+
+    hero.classList.add("show");
+
+}
+        }
+
+
+        if(music){
+
+            music.volume = 0.5;
+
+            music.play();
+
+            if(musicBtn){
+
+                musicBtn.innerHTML="⏸";
+
+            }
+
+        }
+
+
+    });
+
+}
+
+
+
+// MUSIC BUTTON
+
+let playing = true;
+
+
+if(musicBtn){
+
+    musicBtn.addEventListener("click",()=>{
+
+
+        if(!music) return;
+
+
+        if(playing){
+
+            music.pause();
+
+            musicBtn.innerHTML="🎵";
+
+            playing=false;
+
+        }
+        else{
+
+            music.play();
+
+            musicBtn.innerHTML="⏸";
+
+            playing=true;
+
+        }
+
+
+    });
+
+}
+
+
+
+});
+
+const eventDate = new Date("August 30, 2026 10:00:00").getTime();
+
+setInterval(() => {
+
+    const now = new Date().getTime();
+
+    const distance = eventDate - now;
+
+    if (distance <= 0) return;
+
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.getElementById("days").textContent = days;
+    document.getElementById("hours").textContent = hours;
+    document.getElementById("minutes").textContent = minutes;
+    document.getElementById("seconds").textContent = seconds;
+
+}, 1000);
+
+// =======================
+// GALLERY LIGHTBOX
+// =======================
+
+const images = document.querySelectorAll(".gallery-grid img");
+
+const lightbox = document.getElementById("lightbox");
+
+const lightboxImage = document.getElementById("lightbox-image");
+
+const closeBtn = document.getElementById("close");
+
+images.forEach(image=>{
+
+image.addEventListener("click",()=>{
+
+lightbox.style.display="flex";
+
+lightboxImage.src=image.src;
+
+});
+
+});
+
+closeBtn.addEventListener("click",()=>{
+
+lightbox.style.display="none";
+
+});
+
+lightbox.addEventListener("click",(e)=>{
+
+if(e.target===lightbox){
+
+lightbox.style.display="none";
+
+}
+
+});
+
+// =========================
+// FLOWER PETALS
+// =========================
+
+const petalsContainer = document.getElementById("petals");
+
+function createPetal(){
+
+    const petal = document.createElement("div");
+
+    petal.className = "petal";
+
+    petal.innerHTML = "🌸";
+
+    petal.style.left = Math.random() * window.innerWidth + "px";
+
+    petal.style.animationDuration =
+        (6 + Math.random() * 5) + "s";
+
+    petal.style.fontSize =
+        (18 + Math.random() * 18) + "px";
+
+    petalsContainer.appendChild(petal);
+
+    setTimeout(() => {
+
+        petal.remove();
+
+    },11000);
+
+}
+
+setInterval(createPetal,500);
+
+const reveals = document.querySelectorAll(".reveal");
+
+
+window.addEventListener("scroll",()=>{
+
+
+    reveals.forEach(section=>{
+
+
+        const windowHeight = window.innerHeight;
+
+        const top = section.getBoundingClientRect().top;
+
+
+        if(top < windowHeight - 100){
+
+            section.classList.add("show");
+
+        }
+
+
+    });
+
+
+});
+
+// SMART NAVBAR
+
+const navbar = document.getElementById("navbar");
+
+let lastScroll = 0;
+
+
+window.addEventListener("scroll",()=>{
+
+
+    let currentScroll = window.scrollY;
+
+
+    if(currentScroll > lastScroll && currentScroll > 150){
+
+        // scrolling down
+        navbar.classList.add("hide");
+
+    }
+    else{
+
+        // scrolling up
+        navbar.classList.remove("hide");
+
+    }
+
+
+    lastScroll = currentScroll;
+
+
+});
+
+// MOBILE MENU
+
+const menuBtn = document.getElementById("menuBtn");
+const navLinks = document.getElementById("navLinks");
+
+
+if(menuBtn){
+
+    menuBtn.addEventListener("click",()=>{
+
+        navLinks.classList.toggle("show");
+
+    });
+
+}
+
+
+/// RSVP EMAIL
+
+const sendRSVP = document.getElementById("sendRSVP");
+
+
+if(sendRSVP){
+
+    sendRSVP.onclick = function(){
+
+
+        let name = document.getElementById("guestName").value;
+
+        let attendance = document.getElementById("attendance").value;
+
+        let guests = document.getElementById("guestCount").value;
+
+
+        if(name === "" || attendance === "" || guests === ""){
+
+            alert("Please complete the RSVP form.");
+
+            return;
+
+        }
+
+
+        let email = "gersed.marababol03@gmail.com";
+
+
+        let subject = "Christening RSVP";
+
+
+        let body =
+        "Name: " + name +
+        "\nAttendance: " + attendance +
+        "\nNumber of Guests: " + guests;
+
+
+      window.open(
+"https://mail.google.com/mail/?view=cm&fs=1&to="
++ email
++ "&su="
++ encodeURIComponent(subject)
++ "&body="
++ encodeURIComponent(body)
+);
+
+    };
+
+}
